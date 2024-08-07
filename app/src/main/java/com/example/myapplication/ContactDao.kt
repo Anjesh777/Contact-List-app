3203665f685a13b0f706e183c387ff076713b7ee
Anjesh Mainali <anjeshmainali348@gmail.com>
2024-08-07 22:40:31 +0545

    Local Database using Room v0.1

--- app/src/main/java/com/example/myapplication/ContactDao.kt
+++ app/src/main/java/com/example/myapplication/ContactDao.kt
@@ -14,6 +14,13 @@ interface ContactDao {
     @Delete
     suspend fun deleteContact(contact: Contact)
 
+    @Query("SELECT * FROM contact ORDER BY firstName ASC")
     fun GetContactsOrderByFirstName(): Flow<List<Contact>>
+
+    @Query("SELECT * FROM contact ORDER BY lastName ASC")
     fun GetContactsOrderByLastName(): Flow<List<Contact>>
+
+    @Query("SELECT * FROM contact ORDER BY phoneNumber ASC")
+    fun GetContactsOrderByPhoneNumber(): Flow<List<Contact>>
+
 }
---
+++ app/src/main/java/com/example/myapplication/SortType.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication
+
+enum class SortType {
+    FIRST_NAME,
+    LAST_NAME,
+    PHONE_NUMBER
+
+}
